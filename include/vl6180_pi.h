d98ee2ad4a7a66eb8b336456c3d817456b6f3e45
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-11-30 16:31:36 +0000

    start adapting the code for our PST project

--- include/vl6180_pi.h
+++ include/vl6180_pi.h
@@ -8,6 +8,13 @@ int vl6180_initialise(int device);
 int get_distance(vl6180 handle);
 void set_scaling(vl6180 handle, int scaling);
 
+//hack:add access to lower_level functions
+
+int read_byte(vl6180 handle, int reg);
+void write_byte(vl6180 handle, int reg, char data);
+void write_two_bytes(vl6180 handle, int reg, int data);
+
+
 #ifdef __cplusplus
 }
 #endif
